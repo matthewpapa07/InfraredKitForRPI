b060cb0eeb6b8f740382669c23bbcdac799ea78f
matthewpapa <matthewpapa07@users.noreply.github.com>
2016-06-12 00:34:31 +0000

    Client code

--- GarageDoorOpener/GarageDoorOpenerClient.py
+++ GarageDoorOpener/GarageDoorOpenerClient.py
@@ -1,21 +1,8 @@
-import RPi.GPIO as GPIO
-import time
+import socket
 
-#GPIO.cleanup()
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#t2 = time.clock()
-
-print("Start Program" + str(time.clock()))
-for i in range (0,25):
-#	t1 = time.clock()
-	print(time.time())
-	GPIO.wait_for_edge(3, GPIO.FALLING)
-#	print("To Zero after" + str(t2-t1))
-#	t2 = time.clock()
-	print(time.time())
-	GPIO.wait_for_edge(3, GPIO.RISING)
-# 	print("To One after" + str(t1-t2))
-GPIO.cleanup()
-print("End")
+HOST = 'musicbox'    # The remote host
+PORT = 1337              # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall('Hello, world')
+s.close()
