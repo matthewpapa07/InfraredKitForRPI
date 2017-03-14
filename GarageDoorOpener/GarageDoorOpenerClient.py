67560ab658375d284bf67b596554d62f6dcdea1f
matthewpapa07 <matthewpapa07@gmail.com>
2017-03-14 18:56:40 -0500

    Clean out old code

--- GarageDoorOpener/GarageDoorOpenerClient.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-HOST = 'musicbox'    # The remote host
-PORT = 1337              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.sendall('Hello, world')
-s.close()
